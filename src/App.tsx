c76693516a94e27738d7eed8e81cbe9b09afc0e2
Aldair Barros <aldairbarros@outlook.com>
2025-07-14 18:41:45 -0400

    Initial commit: Clean Vite + React + TypeScript template for EklesiaKonecta

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>EklesiaKonecta</h1>
+      <p>Bem-vindo ao EklesiaKonecta!</p>
+    </div>
+  )
+}
+
+export default App
